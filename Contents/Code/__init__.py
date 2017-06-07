195648aec82590c323052aa61dcfbd702b4c9f89
Sander <sander@plex.tv>
2017-06-08 01:10:12 +0200

    Rewrite/update

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,60 +1,48 @@
-TITLE = 'Dance Trippin'
+TITLE = 'DanceTrippin TV'
 ART = 'art-default.jpg'
 ICON = 'icon-default.jpg'
 
 ####################################################################################################
 def Start():
 
-	Plugin.AddViewGroup('List', viewMode='List', mediaType='items')
-	Plugin.AddViewGroup('InfoList', viewMode='InfoList', mediaType='items')
-
 	ObjectContainer.title1 = TITLE
 	DirectoryObject.thumb = R(ICON)
 	HTTP.CacheTime = CACHE_1HOUR
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.8; rv:22.0) Gecko/20100101 Firefox/22.0'
+	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
 
 ####################################################################################################
 @handler('/video/dancetrippin', TITLE, art=ART, thumb=ICON)
 def MainMenu():
 
-	oc = ObjectContainer(view_group='List')
+	oc = ObjectContainer()
 
-	for category in HTML.ElementFromURL('http://player.dancetrippin.tv/').xpath('//ul[@class="video-categories-list"]//a'):
-		title = category.xpath('./text()')[0]
-		category_id = category.xpath('./@href')[0].strip('#')
-
-		if category_id in ('other'):
-			continue
+	for genre in HTML.ElementFromURL('http://www.dancetrippin.tv/videos/dj-sets').xpath('//div[@id="genrelist"]/div[@class="genre"]/a/text()'):
 
 		oc.add(DirectoryObject(
-			key = Callback(Videos, title=title, category_id=category_id),
-			title = title
+			key = Callback(Videos, genre=genre),
+			title = genre
 		))
 
 	return oc
 
 ####################################################################################################
-@route('/video/dancetrippin/{category_id}')
-def Videos(title, category_id):
+@route('/video/dancetrippin/videos')
+def Videos(genre):
 
-	oc = ObjectContainer(title2=title, view_group='InfoList')
+	oc = ObjectContainer(title2=genre)
 
-	for video in JSON.ObjectFromURL('http://player.dancetrippin.tv/video/list/%s/' % category_id):
+	for video in HTML.ElementFromURL('http://www.dancetrippin.tv/videos/dj-sets').xpath('//div[@id="grid-content"]//a[@class="filter" and contains(., "%s")]/parent::div/parent::div/div[@class="image"]' % (genre)):
 
-		if not video['title']:
-			continue
+		title = video.xpath('./a/@title')[0]
+		url = 'http://www.dancetrippin.tv%s' % (video.get('href'))
+		thumb = video.get('style').split('background-image: url(')[-1].split(')')[0].replace('-thumb', '')
 
-		title = video['title']
-		url = 'http://player.dancetrippin.tv/video/%s/#%s' % (video['slug'], category_id)
-		summary = String.DecodeHTMLEntities(String.StripTags(video['description'])).strip()
-
-		if video['image']:
-			thumb = 'http://player.dancetrippin.tv/media/%s' % video['image']
+		if not thumb.startswith('http://'):
+			thumb = 'http://www.dancetrippin.tv%s' % thumb
 
 		oc.add(VideoClipObject(
 			url = url,
 			title = title,
-			summary = summary,
 			thumb = Resource.ContentsOfURLWithFallback(url=thumb, fallback=ICON)
 		))
 
