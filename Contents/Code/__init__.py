be137434ad15cc5df2d6b51be7855422820a8a5a
Sander <sander@plex.tv>
2018-05-08 19:15:05 +0200

    Rewrite to DanceTelevision

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -1,6 +1,6 @@
-TITLE = 'DanceTrippin TV'
-ART = 'art-default.jpg'
-ICON = 'icon-default.jpg'
+TITLE = "DanceTelevision"
+ART = "art-default.jpg"
+ICON = "icon-default.jpg"
 
 ####################################################################################################
 def Start():
@@ -8,15 +8,22 @@ def Start():
 	ObjectContainer.title1 = TITLE
 	DirectoryObject.thumb = R(ICON)
 	HTTP.CacheTime = CACHE_1HOUR
-	HTTP.Headers['User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+	HTTP.Headers['User-Agent'] = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13_3) AppleWebKit/604.5.6 (KHTML, like Gecko) Version/11.0.3 Safari/604.5.6"
 
 ####################################################################################################
-@handler('/video/dancetrippin', TITLE, art=ART, thumb=ICON)
+@handler('/video/dancetelevision', TITLE, art=ART, thumb=ICON)
 def MainMenu():
 
 	oc = ObjectContainer()
+	html = HTML.ElementFromURL('http://www.dancetelevision.net/videos')
 
-	for genre in HTML.ElementFromURL('http://www.dancetrippin.tv/videos/dj-sets').xpath('//div[@id="genrelist"]/div[@class="genre"]/a/text()'):
+	for genre in html.xpath('//div[@id="genrelist"]/div[@class="genre"]/a/text()'):
+
+		videos = html.xpath('//div[@id="grid-content"]//a[@class="filter" and contains(., "{}")]/parent::div/parent::div/div[@class="image"]'.format(genre))
+
+		if len(videos) < 1:
+			Log(" --> Genre '{}' does not contain videos, skipping.".format(genre))
+			continue
 
 		oc.add(DirectoryObject(
 			key = Callback(Videos, genre=genre),
@@ -26,19 +33,19 @@ def MainMenu():
 	return oc
 
 ####################################################################################################
-@route('/video/dancetrippin/videos')
+@route('/video/dancetelevision/videos')
 def Videos(genre):
 
 	oc = ObjectContainer(title2=genre)
 
-	for video in HTML.ElementFromURL('http://www.dancetrippin.tv/videos/dj-sets').xpath('//div[@id="grid-content"]//a[@class="filter" and contains(., "%s")]/parent::div/parent::div/div[@class="image"]' % (genre)):
+	for video in HTML.ElementFromURL('http://www.dancetelevision.net/videos').xpath('//div[@id="grid-content"]//a[@class="filter" and contains(., "{}")]/parent::div/parent::div/div[@class="image"]'.format(genre)):
 
 		title = video.xpath('./a/@title')[0]
-		url = 'http://www.dancetrippin.tv%s' % (video.get('href'))
+		url = 'http://www.dancetelevision.net{}'.format(video.get('href'))
 		thumb = video.get('style').split('background-image: url(')[-1].split(')')[0].replace('-thumb', '')
 
 		if not thumb.startswith('http://'):
-			thumb = 'http://www.dancetrippin.tv%s' % thumb
+			thumb = 'http://www.dancetelevision.net{}'.format(thumb)
 
 		oc.add(VideoClipObject(
 			url = url,
@@ -47,6 +54,6 @@ def Videos(genre):
 		))
 
 	if len(oc) < 1:
-		return ObjectContainer('Empty', 'This directory is empty')
+		return ObjectContainer(header="Empty", message="This directory is empty")
 	else:
 		return oc
